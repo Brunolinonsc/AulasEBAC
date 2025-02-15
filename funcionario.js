9d1e095cda74bdf4a5e74b1225cc374071259ae2
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-02-15 20:56:34 -0300

    õk

--- funcionario.js
+++ funcionario.js
@@ -6,19 +6,44 @@ function Pessoa(nome){
     this.dizCargo = function(){
         console.log(this.cargo)
     }
+    
 }
 
 
 function Funcionario(nome, cargo, salario){
     this.cargo = cargo;
-    this.salario = salario;
+    // this.salario = salario;
+    let _salario = salario;
 
-    Pessoa.call(this, nome);
+    // getters e setters
+
+    this.getSalario = function(){
+        return _salario;
+    }
+
+    this.setSalario = function(valor){
+        if (typeof valor ==='number'){
+            _salario = valor
+        }
+    }
+    Pessoa.call(this, nome)
 }
 
+function Estagiario(nome){
+    Funcionario.call(this, nome, 'Estagiario', 2000)
+
+    this.aumento = function(){
+        const novoSalario = this.getSalario * 1.07;
+        this.setSalario(novoSalario)
+    }
+}
+
+
 const funcionario1 = new Funcionario('Bruno', 'dev font-end', 5000);
-funcionario1.dizoi();
-funcionario1.dizCargo();
+const funcionario2 = new Estagiario('Pedro')
 
+funcionario1.aumento();
+console.log(funcionario1.getSalario())
 
-
+funcionario2.aumento();
+console.log(funcionario2.getSalario())
