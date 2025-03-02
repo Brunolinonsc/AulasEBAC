6f43850531ed1a18ab7d09e81d547774a9bc9104
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-03-02 12:32:51 -0300

    Inicio do modulo vue-js

---
+++ vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  const nome = "Bruno Lino Nascimento"
+  const meuObj = {
+    nome: "Bruno",
+    filmeFavorito: "Os mercenarios"
+  }
+function dizOla(nome) {
+  return `${nome} diz oi`
+}
+const enderecoDaImagenDoBatiman = "https://images.hdqwalls.com/wallpapers/batman-art2020-lc.jpg"
+
+const botaoEstaDesabilitado = false
+
+</script>
+
+<template>
+  <h1>{{dizOla("Bruno")}}</h1>
+  <img :src="enderecoDaImagenDoBatiman" alt="" >
+
+  <button :disabled="!botaoEstaDesabilitado">Enviar Mensagem</button>
+</template>
+
+<style scoped>
+
+</style>
