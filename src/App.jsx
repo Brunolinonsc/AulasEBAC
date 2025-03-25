8bccfaf788a539cff1e0a40ef60c49e6194fc85f
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-03-25 20:10:04 -0300

    commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ import ReposList from "./components/ReposList/indes";
 function App() {
   const[formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
 
+  const [nomeUsuario, setNomeUsuario] = useState('');
 
   return(
     <>
-    <Perfil nome="Bruno" endereco="https://github.com/Brunolinonsc.png"/>
-    <ReposList/>
+    <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+
+    {nomeUsuario.length > 4 && (
+      <>
+        <Perfil nomeUsuario={nomeUsuario}/>
+        <ReposList nomeUsuario={nomeUsuario}/>
+      </>
+    )}
     
     {/* {formularioEstaVisivel && (
       <Formulario />
