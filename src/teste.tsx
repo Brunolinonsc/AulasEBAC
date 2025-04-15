587d3f6dd00c8efa5d5d328705a340707d4ef440
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-04-15 20:55:36 -0300

    Configurando o ambiente de desenvolvimento

--- src/teste.tsx
+++ src/teste.tsx
@@ -1,11 +1,23 @@
 import styled from 'styled-components'
 
-const Botao = styled.button`
-  background-color:red
-`
-
-function Teste () {
-  return <Botao>Clique aqui</Botao>
+type BotaoProps = {
+  principal: boolean
 }
 
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+`
+
+const BotaoPerigo = styled(Botao)`
+background-color: red
+color: #fff
+`
+function Teste() {
+  return (
+    <>
+      <Botao principal>Enviar</Botao>
+      <Botao principal={false}>Cancelar </Botao>
+    </>
+  )
+}
 export default Teste
