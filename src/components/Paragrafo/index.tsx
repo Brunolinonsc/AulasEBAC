7d7cf64efc191402436d6e0d49183fbd16e0e112
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-04-17 15:21:36 -0300

    Criação de components

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -1,9 +1,10 @@
 import { P } from './styles'
 export type Props = {
   children: string
+  tipo?: 'principal' | 'secundario'
 }
 
-const Paragrafo = (props: Props) => (
-  <P fontSize={props.fontSize}>{props.children}</P>
+const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
+  <P tipo={tipo}>{children}</P>
 )
 export default Paragrafo
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
 
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre min</Titulo>
-    <p>
+    <Paragrafo tipo="secundario">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. A necessitatibus,
       laborum veniam asperiores eveniet reiciendis accusantium tempore, dolores
       amet ab nam dicta, doloribus inventore enim. Explicabo accusamus quod
       distinctio hic.
-    </p>
+    </Paragrafo>
   </section>
 )
 export default Sobre
