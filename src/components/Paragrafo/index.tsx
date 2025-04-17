b907cfff8a95a99f1f76931c64f7ca0eacd97685
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-04-17 15:06:01 -0300

    ok

---
+++ src/components/Paragrafo/index.tsx
@@ -0,0 +1,9 @@
+import { P } from './styles'
+export type Props = {
+  children: string
+}
+
+const Paragrafo = (props: Props) => (
+  <P fontSize={props.fontSize}>{props.children}</P>
+)
+export default Paragrafo
---
+++ src/components/Paragrafo/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { Props } from '.'
+
+export const P = styled.p<Props>`
+  font-size: 14px;
+`
