2ea8ce440d1947316fcde783c7acc913588aef93
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-04-19 09:56:12 -0300

    Estilizando a sidebar.

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Brunolinonsc.png" />
-    <Titulo fontSize={20}>Bruno Lino Nascimento</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Bruno Lino Nascimento</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Brunolinonsc
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 export default Sidebar
