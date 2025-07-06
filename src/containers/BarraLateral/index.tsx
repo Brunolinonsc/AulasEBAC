d999824363cc808699380b5ac3fc1699b1b3c55a
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-07-06 19:51:31 -0300

    Atualização de código de aula

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -6,12 +6,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscas" />
       <S.Filtro>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard legenda="pendentes" contador={1} />
+        <FiltroCard legenda="concluidas" contador={2} />
+        <FiltroCard legenda="urgentes" contador={3} />
+        <FiltroCard legenda="importantes" contador={4} />
+        <FiltroCard legenda="normal" contador={5} />
+        <FiltroCard legenda="todas" contador={6} ativo />
       </S.Filtro>
     </div>
   </S.Aside>
