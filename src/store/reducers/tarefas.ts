502adc4b6f768076ece40777369d15fd4ad9913b
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-07-13 10:26:21 -0300

    Aula de inclusão do codigo de remoção de tarefa

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -1,34 +1,41 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import * as enums from '../../utils/enums/Tarefa'
 import Tarefa from '../../models/Tarefa'
+
+type TarefasState = {
+  itens: Tarefa[]
+}
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar javascript',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar'
+    },
+    {
+      id: 2,
+      descricao: 'Não esquecer de passar no mercado.',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Compras'
+    },
+    {
+      id: 3,
+      descricao: 'Efetuar a troca do oleo do motor do carro',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Compras para o carro'
+    }
+  ]
+}
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar Java',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new Tarefa(
-      'Estudar Python',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Assistir as aulas de paython no site da udemy',
-      1
-    ),
-    new Tarefa(
-      'Pagar Cartão de Credito',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Pagar a fatura do cartão de credito que vencera no dia 14/07/2025',
-      1
-    )
-  ],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
     }
   }
 })
