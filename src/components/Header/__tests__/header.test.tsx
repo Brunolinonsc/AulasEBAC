8ccd92ab0a87c648c374aecc6310a67c82c02ae5
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-08-05 20:13:25 -0300

    Termino de aula

--- src/components/Header/__tests__/header.test.tsx
+++ src/components/Header/__tests__/header.test.tsx
@@ -1,15 +1,40 @@
-import { render, screen } from '@testing-library/react'
-import Header from '../index'
-import { Provider } from 'react-redux'
-import { store } from '../../../Store'
+import { screen } from '@testing-library/react'
+import { renderizaComProvider } from '../../../Utils/tests'
+import Header from '..'
+import { Titulo } from '../../Produto/styles'
+
 describe('Testes para o componente header', () => {
   test('Deve renderizar corretamente', () => {
-    const { debug } = render(
-      <Provider store={store}>
-        <Header />
-      </Provider>
-    )
-    debug()
+  renderizaComProvider(<Header />)
     expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+  test('Deve renderizar com 2 itens no carrinho', () => {
+    renderizaComProvider(<Header/>,{
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              categoria: 'RPG',
+              imagem: '',
+              plataforma: ['windows'],
+              preco: 250.9,
+              precoAntigo: 200,
+              titulo:'Elden Ring'
+            },
+            {
+              id: 2,
+              categoria: 'RPG',
+              imagem: '',
+              plataforma: ['windows'],
+              preco: 250.9,
+              precoAntigo: 200,
+              titulo:'Elden Ring'
+            }
+          ]
+        }
+      }
+    })
+    expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
   })
 })
