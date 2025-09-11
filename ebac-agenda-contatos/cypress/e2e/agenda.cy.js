3fc2ac5635ce151d42d691d28bb1f93a3b714b65
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-09-11 19:35:41 -0300

    iniciando a atividade

--- ebac-agenda-contatos/cypress/e2e/agenda.cy.js
+++ ebac-agenda-contatos/cypress/e2e/agenda.cy.js
@@ -1,47 +1,22 @@
 /// <reference types="cypress" />
+describe('Testes para a pagina de candidatura' , () => {
+    beforeEach(() => {
+        cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
+    })
 
-describe('Testes de funcionalidades de Agenda de Contatos', () => {
-  beforeEach(() => {
-    cy.visit('https://agenda-contatos-react.vercel.app/')
-  })
+    it('Deve incluir um contato', () => {
+        cy.get('.Vaga_vagaLink__DeFkk').first().click()
+        cy.get('input[name="nome-completo"]').type('Bruno lino')
+        cy.get('input[name="email"]').type('Bruno@lino.com')
+        cy.get('input[name="telefone"]').type('123456789')
+        cy.get('input[name="endereco"]').type('rua 123 centro')
+        cy.get('#linux').check()
+        cy.get('select[name="escolaridade"]').select('outros')
+        cy.get('.Aplicacao_button__tw2AE').click()
 
-  //Testar funcionalidades: adição, edicão e remoção de contatos
-
-  //Adicionar novo contato
-  it('Deve adicionar novo contato', () => {
-    cy.get('input[type="text"]').type('Isabela Ramos')
-    cy.get('input[type="email"]').type('isabelaramos@teste.com')
-    cy.get('input[type="tel"]').type('11 912345678')
-    cy.get('.adicionar').click()
-
-    cy.contains('div.contato', 'Isabela Ramos').should('exist')
-    cy.contains('div.contato', 'isabelaramos@teste.com').should('exist')
-    cy.contains('div.contato', '11 912345678').should('exist')
-  })
-
-  //Editar informações de contato
-  it('Deve ativar modo de edição', () => {
-    cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
-    cy.get('[type="text"]').should('have.length.greaterThan', 0)
-  })
-
-  it('Deve alterar o primeiro contato selecionado', () => {
-    cy.get('.edit').last().click()
-  
-    //Alterar e-mail
-    cy.get('[type="email"]').clear()
-    cy.get('[type="email"]').type('isabela@teste.com')
-
-    //Alterar telefone
-    cy.get('[type="tel"]').clear()
-    cy.get('[type="tel"]').type('11 989765432')
-
-    //Salvar alterações
-    cy.get('.alterar').click()
-  })
-
-  //Deletar contato
-  it('Deve apagar o ultimo contato', () => {
-    cy.get('.delete').last().click()
-  })
-});
+        cy.on('window:alert', (conteudo) => {
+            expect(conteudo).contain('Obrigado pela candidatura!')
+        })
+        cy.screenshot('tela-incrição-preenchido')
+    })
+})
