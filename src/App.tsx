c948334804c9073b3f681d1408337e2e12b974f5
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2026-04-17 21:31:45 -0300

    Projeto 6 inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { BrowserRouter } from 'react-router-dom'
-
-import Header from './components/Header'
-import { GlobalCss } from './styles'
-
-import Rotas from './routes'
-import Footer from './components/Footer'
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   )
 }
 
